6aec6901225cebacbbb4398b61f8145dd7006d08
BusterBlade <howkyo@live.cn>
2019-11-27 17:38:35 +0800

    C

--- app/src/main/java/com/sh/MainActivity.java
+++ app/src/main/java/com/sh/MainActivity.java
@@ -2,7 +2,16 @@ package com.sh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.opengl.GLES30;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+
+import com.sh.view.gltexture.GLTextureView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GLTextureView tv = findViewById(R.id.tv);
+        tv.setOpaque(false);
+        tv.setEGLContextClientVersion(3);
+        tv.setRenderer(new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+            }
+
+            @Override
+            public void onDrawFrame(GL10 gl) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/sh/view/gltexture/Trace.java
@@ -0,0 +1,20 @@
+package com.sh.view.gltexture;
+
+/**
+ * Created by ssh on 2019/11/27.
+ */
+public class Trace {
+    //todo
+    public static final int TRACE_TAG_VIEW = 1;
+
+    public static void traceBegin(){
+        
+    }
+
+    public static void traceBegin(Object traceTagView, String onSurfaceChanged) {
+        
+    }
+
+    public static void traceEnd(int traceTagView) {
+    }
+}
