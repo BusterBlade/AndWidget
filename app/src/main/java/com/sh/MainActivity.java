ee0b2d64a3f17be869ac18bfe8d2dd61b631d2b2
BusterBlade <howkyo@live.cn>
2019-11-27 18:08:29 +0800

    update

--- app/src/main/java/com/sh/MainActivity.java
+++ app/src/main/java/com/sh/MainActivity.java
@@ -11,7 +11,23 @@ import com.sh.view.gltexture.GLTextureView;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import static android.opengl.GLES10.GL_ALPHA_TEST;
+import static android.opengl.GLES10.GL_TRIANGLE_FAN;
+import static android.opengl.GLES10.glAlphaFunc;
+import static android.opengl.GLES20.GL_ALPHA;
+import static android.opengl.GLES20.GL_ALPHA_BITS;
+import static android.opengl.GLES20.GL_BLEND;
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_DEPTH_BUFFER_BIT;
+import static android.opengl.GLES20.GL_DEPTH_TEST;
+import static android.opengl.GLES20.GL_GREATER;
+import static android.opengl.GLES20.GL_ONE_MINUS_SRC_ALPHA;
+import static android.opengl.GLES20.GL_SRC_ALPHA;
+import static android.opengl.GLES20.glBlendFunc;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glEnable;
+import static android.opengl.GLES20.glViewport;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,16 +41,25 @@ public class MainActivity extends AppCompatActivity {
         tv.setRenderer(new GLSurfaceView.Renderer() {
             @Override
             public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-
+                glClearColor(0f,0f,0f,0.3f);
             }
 
             @Override
             public void onSurfaceChanged(GL10 gl, int width, int height) {
-
+                glViewport(0,0,width,height);
             }
+
+
 
             @Override
             public void onDrawFrame(GL10 gl) {
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                glEnable(GL_BLEND);
+                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+                glEnable(GL_ALPHA_TEST);
+                glAlphaFunc(GL_GREATER,0f);
 
             }
         });
